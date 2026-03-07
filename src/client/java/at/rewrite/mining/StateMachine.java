1a173aa45e0e3c62cfc8631eafd14651fdae257b
a64a <qkcpbie@gmail.com>
2026-03-07 22:33:33 +0100

    Working on new GUI

--- src/client/java/at/rewrite/mining/StateMachine.java
+++ src/client/java/at/rewrite/mining/StateMachine.java
@@ -1,6 +1,5 @@
 package at.rewrite.mining;
 
-import at.rewrite.ConfigManager;
 import at.rewrite.utils.GeneralUtils;
 import at.rewrite.utils.PlayerUtils;
 import at.rewrite.utils.WorldUtils;
@@ -20,8 +19,8 @@ public class StateMachine {
     private static State state = State.IDLE;
     private static BlockPos target;
     public static boolean enabled = false;
-    private static MinecraftClient client = GeneralUtils.getClient();
-    public static String[] blocks = ConfigManager.config.targetBlocks.toArray(new String[0]);
+    public static String[] blocks = new String[]{};
+    private static final MinecraftClient client = GeneralUtils.getClient();
 
     public static void start() {
         enabled = !enabled;
@@ -32,14 +31,15 @@ public class StateMachine {
     }
 
     public static void tick() {
-        if (!enabled)
-            return;
+        if (!enabled) return;
 
         switch (state) {
             case IDLE -> state = State.FINDING_BLOCK;
 
             case FINDING_BLOCK -> {
-                target = WorldUtils.findBlock(5, blocks);
+                assert client.player != null;
+                double reach = client.player.getAttributeValue(EntityAttributes.BLOCK_INTERACTION_RANGE);
+                target = WorldUtils.findBlock((int) reach, blocks);
                 if (target != null) {
                     state = State.LOOKING_AT;
                 } else {
@@ -65,14 +65,12 @@ public class StateMachine {
             case DONE -> {
                 target = null;
                 state = State.IDLE;
-                PlayerUtils.resetVelocity();
             }
         }
     }
 
     public static void tickCamera(float deltaTime) {
-        if (!enabled)
-            return;
+        if (!enabled) return;
         if (state == State.LOOKING_AT && target != null) {
             PlayerUtils.lookAt(null, target, deltaTime);
         }
@@ -80,9 +78,7 @@ public class StateMachine {
 
     private static boolean isLookingAtTarget() {
         for (String block : blocks) {
-            if (PlayerUtils.isLookingAt(block)) {
-                return true;
-            }
+            if (PlayerUtils.isLookingAt(block)) return true;
         }
         return false;
     }
